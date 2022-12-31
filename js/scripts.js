326052c1c3490ebfdc03103d2610810a2b224817
mfflynnnn <89746780+mfflynnnn@users.noreply.github.com>
2022-12-30 20:59:44 -0800

    Converted js script for (f) crossOut to jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,22 @@
 function newItem() {
-//1. Adding a new item to the list of items:
+  //1. Adding a new item to the list of items:
   let li = $("<li></li>");
   let inputValue = $("input").value();
   let text = document.createTextNode(inputValue);
   li.appendChild(text);
 
   if (inputValue === "") {
-      alert("You must write something!");
+    alert("You must write something!");
   } else {
-      $("#list").append("li");
+    $("#list").append("li");
   }
-}
-
-  //   //2. Crossing out an item from the list of items:
-  //   function crossOut() {
-  //     li.classList.toggle("strike");
-  //   }
-
-  //   li.addEventListener("dblclick", crossOut);
 
   //2. Crossing out an item from the list of items:
-  function crossOut {
-      return null;
+  function crossOut() {
+    li.toggleClass("strike");
   }
+
+  li.on("dblclick", crossOut);
 
   //   //3(i). Adding the delete button "X":
   //   let crossOutButton = document.createElement("crossOutButton");
@@ -36,3 +30,4 @@ function newItem() {
   //   }
   //   // 4. Reordering the items:
   //   $("#list").sortable();
+}
